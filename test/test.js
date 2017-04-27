1b5897066812185cb48f91664ef6a8e2856946f5
Raul Martin <alu0100769579@ull.edu.es>
2017-04-27 16:27:05 +0000

    Arreglando test

--- test/test.js
+++ test/test.js
@@ -6,12 +6,12 @@ let Rectangle = require('../rectangle.js');
 require("should");
 
 describe("getArea", function() {
-  it("must compute the triangle area correctly. Shape object", function() {
+  it("must compute the rectangle area correctly. Shape object", function() {
     let a = new Shape({ width: 100, height: 50 }, 'Rectangle');
     let s = a.getArea();
     s.should.equal(5000);
   })
-  it("must compute the triangle area correctly. Rectangle object", function() {
+  it("must compute the rectangle area correctly. Rectangle object", function() {
     let a = new Rectangle({ width: 100, height: 50 });
     let s = a.getArea();
     s.should.equal(5000);
