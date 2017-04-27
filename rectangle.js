abf7011fa2b46a6796aa83c30ed844fce0e0fa00
Raul Martin <alu0100769579@ull.edu.es>
2017-04-27 17:58:56 +0000

    Añadiendo documentacion

--- rectangle.js
+++ rectangle.js
@@ -2,10 +2,34 @@
 
 let Shape = require('@alu0100769579/ull-shape-alu0100769579');
 
+/**
+ *Paquete npm de Rectangle
+ * 
+ * Autor: Raúl Martín Morales
+ * 
+ * Aquí creamos la clase Rectangle que hereda de Shape
+ * 
+ * @class Rectangle
+ * @param {hash} options Ancho y Alto
+ * 
+ */
+ 
 class Rectangle extends Shape {
+  
+    /**
+    * @construct Rectangle
+    * @param{hash} options Ancho y Alto
+    */
+    
     constructor(options) {
       super(options)
     }
+    
+    /**
+    * @function getArea
+    * @return {value} retorna el valor del area del rectangle
+    */
+    
     area() {
       return this.width * this.height;
     }
